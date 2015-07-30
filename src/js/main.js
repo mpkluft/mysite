7ed7a1ea180a43564690420a226a7eb79c02bd6d
Alex <mpkluft@yandex.ru>
2015-07-30 22:38:40 +0300

    gulp assembley

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+/*
+ * Third party
+ */
+ 
+//= ../../bower_components/html5shiv/dist/html5shiv.js
+//= ../../bower_components/jquery/dist/jquery.js
+
+/*
+ * Custom
+ */
+//= partials/app.js
---
+++ src/js/partials/app.js
@@ -0,0 +1,4 @@
+// протестируем данную функцию
+function isEven(val) {
+	return val % 2 === 0;
+}
