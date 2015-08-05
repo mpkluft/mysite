c2c946e92ec7b48e3d32b2366f035b3a4796193f
Alex <mpkluft@yandex.ru>
2015-08-06 01:00:24 +0300

    вечерний коммит

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,8 @@
  
 //= ../../bower_components/html5shiv/dist/html5shiv.js
 //= ../../bower_components/jquery/dist/jquery.js
+//= ../..//bower_components/bootstrap/dist/js/bootstrap.min.js
+
 
 /*
  * Custom
